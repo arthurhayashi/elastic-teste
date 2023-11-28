7b522321ee42c8bc4415fb536ecdfcf89e9a1ba3
Arthur Yukio Hayashi <arthuryukiohayashi@gamil.com>
2023-11-28 10:39:08 -0300

    add-att-view

--- src/index.php
+++ src/index.php
@@ -1,30 +1,98 @@
 <?php
 // index.php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $logstashHost = 'logstash'; // Nome do serviço Logstash no docker-compose
 $logstashPort = '5000';     // Porta configurada para o input HTTP do Logstash
 
+$esHost = 'elasticsearch'; // Endereço do Elasticsearch
+$esPort = '9200';          // Porta padrão do Elasticsearch
+$index = 'meu_indice';     // Nome do índice no Elasticsearch
+
+// Verificar o método da requisição e a ação
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $client = curl_init("http://$logstashHost:$logstashPort/");
+    $action = $_POST['action'] ?? '';
 
-    $data = json_encode(['message' => $_POST['message']]);
+    if ($action === 'send') {
+        // Enviar dados para o Logstash
+        $client = curl_init("http://$logstashHost:$logstashPort/");
+        $data = json_encode(['message' => $_POST['message']]);
+        curl_setopt($client, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($client, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($client, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data)
+        ]);
+        $response = curl_exec($client);
+        curl_close($client);
+        echo "Data sent to Logstash: $response";
+    } elseif ($action === 'update') {
+        // Atualizar documento no Elasticsearch
+        $documentId = $_POST['documentId'];
+        $message = $_POST['message'];
+        $updatedData = json_encode(['doc' => ['message' => $message]]);
+        $updateUrl = "http://$esHost:$esPort/$index/_update/$documentId";
+        $ch = curl_init($updateUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $updatedData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($updatedData)
+        ]);
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        curl_close($ch);
+        echo "Resposta do Elasticsearch: " . $response;
+    }
+}
 
-    curl_setopt($client, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($client, CURLOPT_POSTFIELDS, $data);
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET['documentId'])) {
+    $searchUrl = "http://$esHost:$esPort/$index/_doc/" . $_GET['documentId'];
+
+    $client = curl_init($searchUrl);
     curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($client, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen($data)
-    ]);
 
     $response = curl_exec($client);
-    curl_close($client);
+    if ($response === false) {
+        echo "cURL Error: " . curl_error($client);
+    } else {
+        $responseArray = json_decode($response, true);
 
-    echo "Data sent to Logstash: $response";
+        if (isset($responseArray['found']) && $responseArray['found']) {
+            // Usando json_encode com JSON_PRETTY_PRINT
+            echo "<pre>Documento encontrado: " . json_encode($responseArray['_source'], JSON_PRETTY_PRINT) . "</pre>";
+        } else {
+            echo "Documento não encontrado.";
+        }
+    }
+
+    curl_close($client);
 }
+
 ?>
 
 <form method="post">
+    <input type="hidden" name="action" value="send">
     <input type="text" name="message">
-    <button type="submit">Send</button>
+    <button type="submit">Enviar para Logstash</button>
 </form>
+
+<form method="post">
+    <input type="hidden" name="action" value="update">
+    <input type="text" name="documentId" placeholder="ID do Documento">
+    <input type="text" name="message" placeholder="Nova Mensagem">
+    <button type="submit">Atualizar Documento</button>
+</form>
+
+<form method="get">
+    <input type="text" name="documentId" placeholder="ID do Documento">
+    <button type="submit">Buscar Documento</button>
+</form>
+
